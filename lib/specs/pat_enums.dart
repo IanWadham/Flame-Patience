2b1024f03a1fac16c3cf9cdeb86e8d3bfef97ea4
IanWadham <iandw.au@gmail.com>
2024-01-07 17:06:21 +1100

    fix: Separate specs/pat_enums.dart file from specs/pat_specs.dart.

---
+++ lib/specs/pat_enums.dart
@@ -0,0 +1,60 @@
+enum PatGameID {
+  klondikeDraw1,
+  klondikeDraw3,
+  fortyAndEight,
+  mod3,
+  noSuchGame,
+}
+
+enum PileType {
+  stock,
+  waste,
+  tableau,
+  foundation,
+}
+
+enum MoveResult {
+  valid,
+  pileEmpty,
+  notValid,
+}
+
+enum DealSequence {
+  notUsed,
+  pilesInTurn,
+  wholePileAtOnce,
+}
+
+enum DragRule {
+  dragNotAllowed,
+  fromTop,
+  fromAnywhere,
+}
+
+enum PutRule {
+  putNotAllowed,
+  ascendingSameSuitBy1,
+  ascendingSameSuitBy3,
+  descendingSameSuitBy1,
+  descendingAlternateColorsBy1
+}
+
+enum TapRule {
+  tapNotAllowed,
+  turnOver1,
+  turnOver3,
+  goOut,
+}
+
+enum TapEmptyRule {
+  tapNotAllowed,
+  turnOverWasteOnce,
+  turnOverWasteUnlimited,
+}
+
+enum DealFaceRule {
+  notUsed,
+  faceDown,
+  faceUp,
+  lastFaceUp,
+}
