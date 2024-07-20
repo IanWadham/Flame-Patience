fd1455ccf168c028c7b483dc27c6d006c40bab39
IanWadham <iandw.au@gmail.com>
2024-07-20 18:10:08 +1000

    fix: Remove or comment out debug-code in models/card_moves.dart.

--- lib/models/card_moves.dart
+++ lib/models/card_moves.dart
@@ -45,14 +45,12 @@ class CardMoves {
     int leadCard = 0,
     int strength = 0,
   }) {
-    // print('MOVE LIST before storeMove() index $_redoIndex:'); printMoves();
     if (_redoIndex < _playerMoves.length) {
       // Remove range from redoIndex to end.
       _playerMoves.removeRange(_redoIndex, _playerMoves.length);
-      // print('MOVE LIST after PRUNING, index $_redoIndex:'); printMoves();
     }
     assert(nCards > 0, 'storeMove(): BAD VALUE OF nCards = $nCards.');
-    // TODO - This needs to happen in UNDO as well and maybe also REDO.
+    // TODO - The next code needs to happen in UNDO as well and maybe also REDO.
     // TODO - Do we need a better way to do this? Keep returned List<CardView>?
     // TODO - Highlighting would not be needed in the Solver logic.
     for (CardView card in _cards) {
@@ -68,20 +66,13 @@ class CardMoves {
     );
     _playerMoves.add(move);
     _redoIndex = _playerMoves.length;
-    // print('MOVE LIST after storeMove() index $_redoIndex:'); printMoves();
-    // print ('Stored Move: ${move.fromPile} ${move.toPile} n${move.nCards} '
-          // '${_cards[move.leadCard]} e${move.extra.index}');
-    // print('Move: ${from.pileIndex} ${from.pileType} to ${to.pileIndex} '
-        // '${to.pileType} $nCards cards ${_cards[leadCard]} $extra');
   }
 
   UndoRedoResult undoMove() {
     if (_redoIndex < 1) {
       return UndoRedoResult.atStart;
     }
-    // print('MOVE LIST before undoMove() index $_redoIndex:'); printMoves();
     return moveBack(_playerMoves[--_redoIndex]);
-    // print('MOVE LIST after undoMove() index $_redoIndex:'); printMoves();
   }
 
   UndoRedoResult redoMove() {
@@ -89,9 +80,7 @@ class CardMoves {
     if (_redoIndex >= _playerMoves.length) {
       return UndoRedoResult.atEnd;
     }
-    // print('MOVE LIST before redoMove() index $_redoIndex:'); printMoves();
     return makeMove(_playerMoves[_redoIndex++]);
-    // print('MOVE LIST after redoMove() index $_redoIndex:'); printMoves();
   }
 
   UndoRedoResult makeMove(CardMove move) {
@@ -102,10 +91,6 @@ class CardMoves {
     assert((_redoIndex >= 0) && (_redoIndex <= _playerMoves.length));
     Pile from = _piles[move.fromPile];
     Pile to = _piles[move.toPile];
-    // print(
-        // '\n\n\n\nREDO: ${move.fromPile} ${from.pileType} to ${move.toPile} '
-       // '${to.pileType} redo $_redoIndex list ${_playerMoves.length} '
-       // '${move.nCards})');
 
     // SPECIAL CASE: Turn Waste Pile over, back onto Stock (tap on empty Stock).
     if ((from.pileType == PileType.waste) && (to.pileType == PileType.stock)) {
@@ -131,12 +116,10 @@ class CardMoves {
         final nMax = move.nCards;
         assert (from.nCards >= nMax);
         // print('Tableau Indices: $_tableaus');
-        from.dump();
         for (final pile in _tableaus) {
           pile.dropCards(from.grabCards(1, reverseAndFlip: true));
           if (++n >= nMax) break;
         }
-        from.dump();
       case Extra.autoDealTableau:
         // From Tableau to Foundation or Excluded + new card dealt from Stock.
         to.dropCards(from.grabCards(1));
@@ -157,9 +140,6 @@ class CardMoves {
     // on the meanings of the switch() cases, see makeMove() above.
     Pile from = _piles[move.fromPile];
     Pile to = _piles[move.toPile];
-    // print('\n\n\nUNDO: ${move.fromPile} ${from.pileType} to ${move.toPile} '
-        // '${to.pileType} undo $_redoIndex list ${_playerMoves.length} '
-        // 'nCards ${move.nCards}');
 
     if ((from.pileType == PileType.waste) && (to.pileType == PileType.stock)) {
       to.turnPileOver(from); // Undo (return cards to Waste Pile from Stock).
@@ -202,7 +182,6 @@ class CardMoves {
     //        turn it over, the pile should NOT be highlighted as a move.
     // TODO - Find first empty Tableau, if any (e.g. for dropping a King).
 
-    // print('ENTER getPossibleMoves()');
     bool canDrawFromStock = false;
     bool fromIsMisfit = false;
     List<CardView> possibleCardsToMove = [];
@@ -218,16 +197,13 @@ class CardMoves {
       }
 
       final cards = fromPile.getCards();
-      // print('Moves in ${fromPile.pileIndex} ${fromPile.pileType} cards $cards');
       fromIsMisfit = ((cards.length == 1) &&
           (fromPile.pileType == PileType.foundation) &&
           (cards.last.rank != fromPile.pileSpec.putFirst));
-      // print('Is fromPile a misfit? $fromIsMisfit: card $cards');
       for (CardView card in cards) {
         final List<CardView> dragList = [];
         if (fromPile.isDragMoveValid(card, dragList, grabbing: false) ==
             MoveResult.valid) {
-          // print('Try card $dragList from ${fromPile.pileIndex}');
           for (Pile toPile in _piles) {
             if ((toPile == fromPile) ||
                 (toPile.pileType == PileType.freecell)) {
@@ -242,13 +218,10 @@ class CardMoves {
                 // nothing. Show the move if the card does not belong on that
                 // Foundation pile and can go out or to an empty Tableau in the
                 // endgame of Mod 3.
-                // print('Show misfit from ${fromPile.pileIndex} '
-                    // 'to ${toPile.pileType}');
                 showMove = true;
               } else if (fromPile.pileType == PileType.foundation) {
                 // If moves from Foundations are possible, don't show them,
                 // unless the card is a misfit (as in Mod 3, see above).
-                // print('Ignore move from Foundation.');
                 showMove = false;
               } else if (toPile.hasNoCards &&
                   (dragList.length == cards.length) &&
@@ -259,7 +232,6 @@ class CardMoves {
                 showMove = false;
               } else {
                 showMove = true;
-                // print('Show all other valid moves');
               }
               if (showMove) {
                 possibleCardsToMove.add(card);
@@ -281,7 +253,6 @@ class CardMoves {
         // If Stock is not empty, highlight it when there are no other moves.
         possibleCardsToMove.add(card);
         card.isHighlighted = true;
-        // print('Stock top $card, possible cards $possibleCardsToMove');
       }
     }
     // print('Possible cards to move $possibleCardsToMove');
@@ -290,22 +261,19 @@ class CardMoves {
 
   void switchTableauStates(int redealNumber) {
     List<List<int>> tableauStates = [];
-    _tableaus[1].dump();
     for (final tableau in _tableaus) {
       tableauStates.add(tableau.restoreState(redealNumber));
     }
-    _tableaus[1].dump();
     int n = 0;
     for (final tableau in _tableaus) {
       tableau.showPileState(tableauStates[n]);
       n++;
     }
-    _tableaus[1].dump();
     tableauStates.clear();
   }
-
+/* For debugging.
   void dump() {
-    // print('DUMP ${_playerMoves.length} MOVES: redo index $_redoIndex');
+    print('DUMP ${_playerMoves.length} MOVES: redo index $_redoIndex');
   }
 
   void printMoves() {
@@ -316,27 +284,5 @@ class CardMoves {
     }
     // print(moves);
   }
-}
-
-/*
-import 'dart:typed_data';
-
-  // EXPERIMENTAL CODE - Might need to use ByteData in proposed Solver.
-
-  final ByteData _b = ByteData(100);
-
-    print('START 1,000,000,000 ${DateTime.now().toString()}');
-    for (int k = 0; k < 1000000000; k++) {
-    for (int n = 1; n <= 100; n++) {
-      _b.setUint8(n - 1, n);
-    }
-    }
-    print('DONE ${DateTime.now().toString()}');
-    // One billion repeats of the inner loop of 100 took 1 min 25 sec.
-    // Happily accepts ByteData size of 1 to 4 billion bytes.
-    // Has Range Checking on setUint8() and presumably getUint8().
-    print('Element length _b ${_b.elementSizeInBytes} ByteData length ${_b.lengthInBytes}');
-    print('_b ByteBuffer size ${_b.buffer.lengthInBytes}');
-    print('_b[41] ${_b.getUint8(41)}');
-    // _b.setUint8(100, 3); // Dart throws a RangeError...
 */
+}
